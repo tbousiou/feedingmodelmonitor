db51990c7d3baf9b34ce2d4bb2d0e567742cfad9
Theodoros Bousiou <tbousiou@gmail.com>
2022-06-29 14:35:39 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+# Custom imports 
+#from multipage import MultiPage
+#from pages import models_map, compare_models, growth_monitor # import your pages here
+
+# Create an instance of the app 
+#app = MultiPage()
+
+# Title of the main page
+st.title("Feed model application")
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
+
+# Add all your applications (pages) here
+# app.add_page("Compare Models Map", compare_models.app)
+# app.add_page("Models Map", models_map.app)
+# app.add_page("Growth Monitor", growth_monitor.app)
+
+
+
+# # The main app
+# app.run()
